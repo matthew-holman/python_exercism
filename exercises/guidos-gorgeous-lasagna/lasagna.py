5eacf3ef6e905793de5fe97ead1a742f58178f17
Matthew Holman <matthewh@matthewacstudio.lan>
2025-05-09 12:09:12 +0200

    update flake8 settings, remove todos

--- exercises/guidos-gorgeous-lasagna/lasagna.py
+++ exercises/guidos-gorgeous-lasagna/lasagna.py
@@ -7,13 +7,10 @@ This is a module docstring, used to describe the functionality
 of a module and its functions and/or classes.
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant below.
 EXPECTED_BAKE_TIME = 40
-
 PREPARATION_TIME_PER_LAYER = 2
 
 
-# TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
 def bake_time_remaining(elapsed_bake_time: int):
     """Calculate the bake time remaining.
 
@@ -28,10 +25,6 @@ def bake_time_remaining(elapsed_bake_time: int):
     return EXPECTED_BAKE_TIME - elapsed_bake_time
 
 
-# TODO: Define the 'preparation_time_in_minutes()' function below.
-# You might also consider defining a 'PREPARATION_TIME' constant.
-# You can do that on the line below the 'EXPECTED_BAKE_TIME' constant.
-# This will make it easier to do calculations.
 def preparation_time_in_minutes(layers: int) -> int:
     """
     :param layers:
@@ -40,7 +33,6 @@ def preparation_time_in_minutes(layers: int) -> int:
     return layers * PREPARATION_TIME_PER_LAYER
 
 
-# TODO: define the 'elapsed_time_in_minutes()' function below.
 def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> int:
     """
     :param number_of_layers: int
@@ -48,7 +40,3 @@ def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> in
     :return: int
     """
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
-
-
-# TODO: Remember to go back and add docstrings to all your functions
-#  (you can copy and then alter the one from bake_time_remaining.)
